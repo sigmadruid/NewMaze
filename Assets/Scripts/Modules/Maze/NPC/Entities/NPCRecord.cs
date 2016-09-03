582cbaf72679823ffef32bfb48e9999d64c050c1
sigmadruid <sigmadruid@qq.com>
2016-09-03 12:00:15 +0800

    【NPC】简化NPC目录结构。 【陷阱】加入陷阱模型。 【UI】修复TransitionPanel的BUG。

--- Assets/Scripts/Modules/Maze/NPC/Entities/NPCRecord.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace GameLogic
-{
-    public class NPCRecord : EntityRecord
-    {
-    }
-}
---
+++ Assets/Scripts/Modules/Maze/Trap/TrapScript.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class TrapScript
+{
+    public TrapScript()
+    {
+    }
+}
+
