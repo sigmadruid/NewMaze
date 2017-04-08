1bc249492ae11cbb8021e44bcb0f4a5c061b6d14
sigmadruid <sigmadruid@qq.com>
2017-04-08 13:39:17 +0800

    HomeTown更新光照贴图

--- Assets/Scripts/Utils/Test.cs
+++ Assets/Scripts/Utils/Test.cs
@@ -5,22 +5,12 @@ using GameUI;
 
 public class Test : MonoBehaviour
 {
-    public BubbleItem bubble;
-
     void Awake()
     {
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            bubble.Show(true, "aaa", "bbb");
-        }
-        if(Input.GetMouseButtonDown(1))
-        {
-            bubble.Show(false, "aaa", "bbb");
-        }
     }
 }
 
