06d7fd7d982712e6fa9a03a658337c53c6f729fb
sigmadruid <sigmadruid@qq.com>
2017-05-08 09:01:10 +0800

    Loading界面优化

---
+++ Assets/Scripts/Effects/UISelfRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+
+public class UISelfRotate : MonoBehaviour
+{
+    public float speed;
+    public bool rotate;
+
+    private RectTransform ui;
+
+    void Awake()
+    {
+        ui = transform as RectTransform;
+    }
+    void Update()
+    {
+        if (rotate)
+            ui.localEulerAngles += Vector3.forward * speed * Time.deltaTime;
+    }
+}
+
