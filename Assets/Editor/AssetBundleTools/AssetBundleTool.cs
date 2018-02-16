e2dcde43bd98caf4eef89db0c2fba718a300ef5f
sigmadruid <sigmadruid@qq.com>
2018-02-16 23:23:48 +0800

    AB-去掉Resosurce目录

---
+++ Assets/Editor/AssetBundleTools/AssetBundleTool.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using System;
+
+public static class AssetBundleTool
+{
+    public const string PREFABS_PATH = "";
+    public const string ASSETS_PATH = "";
+
+    public static void MarkABTags()
+    {
+    }
+
+    public static void BuildAllAB()
+    {
+    }
+}
+
