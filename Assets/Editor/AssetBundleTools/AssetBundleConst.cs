3a889f2b75ef1b838c7c6e389edf1ef55613951c
sigmadruid <sigmadruid@qq.com>
2018-02-17 21:33:46 +0800

    AB-AssetBundleTools

---
+++ Assets/Editor/AssetBundleTools/AssetBundleConst.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class AssetBundleConst
+{
+    public const string PREFABS_PATH = "Assets/PrefabResources";
+    public const string ASSETS_PATH = "Assets/RawResources";
+
+    public const int MAX_AB_SIZE = 10 * 1024 * 1024;
+}
+
--- Assets/Editor/AssetBundleTools/AssetBundleTool.cs
+++ Assets/Editor/AssetBundleTools/AssetBundleTool.cs
@@ -2,18 +2,111 @@
 using UnityEngine;
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 public static class AssetBundleTool
 {
-    public const string PREFABS_PATH = "";
-    public const string ASSETS_PATH = "";
+    private static Dictionary<string, List<string>> filePathDic = new Dictionary<string, List<string>>();
 
+    [MenuItem("Tools/Mark AssetBundle Tags")]
     public static void MarkABTags()
     {
+        ClearAll();
+
+        MarkPrefabTags();
+        MarkAssetsTags();
+
+        AssetDatabase.Refresh();
+    }
+
+    private static void MarkPrefabTags()
+    {
+        string[] folderPathList = Directory.GetDirectories(AssetBundleConst.PREFABS_PATH);
+        for (int i = 0; i < folderPathList.Length; ++i)
+        {
+            string folderPath = folderPathList[i];
+            string folderName = Path.GetFileName(folderPath);
+            List<string> filePathList = new List<string>();
+            filePathDic.Add(folderName, filePathList);
+            GetAllFilePaths(folderPath, filePathList);
+
+            for(int j = 0; j < filePathList.Count; ++j)
+            {
+                string filePath = filePathList[j];
+                if(filePath.Contains(".meta"))
+                {
+                    continue;
+                }
+                var importer = AssetImporter.GetAtPath(filePath);
+                importer.SetAssetBundleNameAndVariant(folderName, string.Empty);
+                Debug.LogErrorFormat("{0}, tag={1}", filePath, folderName);
+            }
+        }
+    }
+
+    private static void MarkAssetsTags()
+    {
+        List<string> allDependencies = new List<string>();
+        foreach(string fileName in filePathDic.Keys)
+        {
+            List<string> filePathList = filePathDic[fileName];
+//            List<string> fileNameList = new List<string>();
+//            for(int j = 0; j < filePathList.Count; ++j)
+//            {
+//                string filePath = filePathList[j];
+//                fileNameList.Add(filePath);
+//            }
+            string[] dependencies = AssetDatabase.GetDependencies(filePathList.ToArray());
+            allDependencies.AddRange(dependencies);
+        }
+
+        for(int i = 0; i < allDependencies.Count; ++i)
+        {
+            string dependency = allDependencies[i];
+            Debug.LogError(dependency);
+        }
     }
 
     public static void BuildAllAB()
     {
     }
+
+    public static void ClearAll()
+    {
+        foreach(var list in filePathDic.Values)
+        {
+            list.Clear();
+        }
+        filePathDic.Clear();
+    }
+
+    #region Helper Functions
+
+    private static void GetAllFilePaths(string rootFolder, List<string> paths)
+    {
+        string[] files = Directory.GetFiles(rootFolder);
+        for(int i = 0; i < files.Length; ++i)
+        {
+            string file = files[i];
+            if(file.Contains(".prefab"))
+            {
+                paths.Add(file);
+            }
+        }
+
+        string[] folders = Directory.GetDirectories(rootFolder);
+        if(folders.Length > 0)
+        {
+            for(int i = 0; i < folders.Length; ++i)
+            {
+                string folder = folders[i];
+                GetAllFilePaths(folder, paths);
+            }
+        }
+    }
+
+    #endregion
 }
 
